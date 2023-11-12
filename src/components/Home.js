fce17372cab588baae8028ebb3686799fb545c76
AUTUMN <661benton@gmail.com>
2023-11-12 13:36:54 -0800

    init project

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>HOME</h1>
+    </div>
+  );
+}
+
+
+export default Home;
