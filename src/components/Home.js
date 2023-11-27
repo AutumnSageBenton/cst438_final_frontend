b70e8534ff07e8c404c75c39054c079c5883e0e3
IvanMart57 <68267157+IvanMart57@users.noreply.github.com>
2023-11-27 14:09:25 -0800

    Home page fix
    Had some extra code from the next To-Do I was working on. Fixed it so home page will render

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import '../App.css';
 import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import styles from '../styles/Home.css';
+import { useRef } from 'react';
+
 
 
 
 function Home() {
 
-  const byNameRef = useRef();
+  const byNameRef = useRef(null);
   
   return (
     <div className="Home">
@@ -16,7 +18,7 @@ function Home() {
       <br/>
       <br/>
       Search by Name:
-      <input name='searchByName' ref={byNameRef}/>
+      <input name='searchByName' ref={byNameRef} type='text'/>
     </div>
   );
 }
